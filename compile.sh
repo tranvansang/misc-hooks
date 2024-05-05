4f42f7fd9ffa17117e98900424720fa3860d2f9a
Sang Tran <t@sang.jp>
2024-05-06 02:31:45 +0900

    yarn to npm

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -rf index.{,m,c}mjs index.d.ts \
-&& yarn tsc -p tsconfig.json \
+&& npx tsc -p tsconfig.json \
 && mv index.js index.mjs \
-&& yarn tsc -p tsconfig.cjs.json \
+&& npx tsc -p tsconfig.cjs.json \
 && mv index.js index.cjs
