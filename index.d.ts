8be67ff21eb3ee851d7bd5f0d3ca779a116f031c
Sang <t@sang.jp>
2024-07-25 13:11:11 +0900

    type

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,22 @@
-import type { Dispatch, MutableRefObject, SetStateAction } from 'react';
+import { Dispatch, MutableRefObject, SetStateAction } from 'react';
 type OptionalArraySub<T extends readonly unknown[], R extends readonly unknown[]> = number extends R['length'] ? never : number extends T['length'] ? never : T['length'] extends R['length'] ? R : OptionalArraySub<T, [...R, undefined | T[R['length']]]>;
 export type OptionalArray<T extends readonly unknown[]> = number extends T['length'] ? (T[number] | undefined)[] : OptionalArraySub<T, []>;
-export declare const nextStateFromAction: <T>(action: SetStateAction<T>, state: T) => T;
-export declare const useToggle: (init?: boolean) => [boolean, (state?: boolean) => void];
-export declare const useTurnOff: () => [boolean, () => void];
-export declare const useTurnOn: () => [boolean, () => void];
-export declare const useUnmountedRef: () => MutableRefObject<boolean>;
-export declare const useMountedRef: () => MutableRefObject<boolean>;
-export declare const useMounted: () => boolean;
-export declare const useTimedOut: (timeout: number) => boolean;
-export declare const useDebounce: <T>(value: T, timeout: number) => T;
-export declare const useDeepMemo: <T>(val: T, isEqual?: (cur: T, next: T) => boolean) => T;
-export declare const useForceUpdate: () => () => void;
-export declare const usePrevRef: <T>(value: T) => MutableRefObject<T | undefined>;
-export declare const useDefaultState: <T>(defaultState: T) => readonly [T, Dispatch<SetStateAction<T>>];
-export declare const useEffectWithPrevDeps: <T extends readonly unknown[]>(effect: (prevDeps: OptionalArray<T>) => (void | (() => void | undefined)), deps: T) => void;
-export declare const useLayoutEffectWithPrevDeps: <T extends readonly unknown[]>(effect: (prevDeps: OptionalArray<T>) => (void | (() => void | undefined)), deps: T) => void;
-export declare const useEffectOnce: <T extends readonly unknown[]>(effect: () => (void | (() => void | undefined)), deps: T) => void;
+export declare function nextStateFromAction<T>(action: SetStateAction<T>, state: T): T;
+export declare function useToggle(init?: boolean): [boolean, (state?: boolean) => void];
+export declare function useTurnOff(): [boolean, () => void];
+export declare function useTurnOn(): [boolean, () => void];
+export declare function useUnmountedRef(): MutableRefObject<boolean>;
+export declare function useMountedRef(): MutableRefObject<boolean>;
+export declare function useMounted(): boolean;
+export declare function useTimedOut(timeout: number): boolean;
+export declare function useDebounce<T>(value: T, timeout: number): T;
+export declare function useDeepMemo<T>(val: T, isEqual?: (cur: T, next: T) => boolean): T;
+export declare function useForceUpdate(): () => void;
+export declare function usePrevRef<T>(value: T): MutableRefObject<T | undefined>;
+export declare function useDefaultState<T>(defaultState: T): readonly [T, Dispatch<SetStateAction<T>>];
+export declare function useEffectWithPrevDeps<T extends readonly unknown[]>(effect: (prevDeps: OptionalArray<T>) => (void | (() => void | undefined)), deps: T): void;
+export declare function useLayoutEffectWithPrevDeps<T extends readonly unknown[]>(effect: (prevDeps: OptionalArray<T>) => (void | (() => void | undefined)), deps: T): void;
+export declare function useEffectOnce<T extends readonly unknown[]>(effect: () => (void | (() => void | undefined)), deps: T): void;
 export declare function useEnhancedState<S>(initialState?: S | (() => S)): [S, Dispatch<SetStateAction<S>>, MutableRefObject<S>];
 export declare function useEnhancedState<S = undefined>(): [
     S | undefined,
@@ -29,9 +29,12 @@ export declare function useRefState<T = undefined>(): [
     Dispatch<SetStateAction<T | undefined>>,
     MutableRefObject<T | undefined>
 ];
-export declare const useAtomicMaker: () => [boolean, <T, V>(cb: (...params: T[]) => V) => ((...params: T[]) => Promise<V | void>)];
-export declare const useAtomicCallback: <T, V extends Promise<any>>(cb: (...params: T[]) => V) => [boolean, (...params: T[]) => Promise<V | void>];
-export declare const useRefValue: <T>(value: T) => MutableRefObject<T>;
+export declare function useAtomicMaker(): [
+    boolean,
+    <T, V>(cb: (...params: T[]) => V) => ((...params: T[]) => Promise<V | void>)
+];
+export declare function useAtomicCallback<T, V extends Promise<any>>(cb: (...params: T[]) => V): [boolean, (...params: T[]) => Promise<V | void>];
+export declare function useRefValue<T>(value: T): MutableRefObject<T>;
 export declare function usePropState<S>(initialState: S | (() => S)): {
     value: S;
     setValue: Dispatch<SetStateAction<S>>;
@@ -40,9 +43,9 @@ export declare function usePropState<S = undefined>(): {
     value: S | undefined;
     setValue: Dispatch<SetStateAction<S | undefined>>;
 };
-export declare const useScopeId: (prefix?: string) => (name?: string) => string;
-export declare const useUpdate: <T>(getValue: (current?: T) => T) => [T, import("react").DispatchWithoutAction];
-export declare const useKeep: <T>(value: T) => T;
+export declare function useScopeId(prefix?: string): (name?: string) => string;
+export declare function useUpdate<T>(getValue: (current?: T) => T): [T, import("react").DispatchWithoutAction];
+export declare function useKeep<T>(value: T): T;
 export declare function useListData<T>({ load, initial, }: {
     initial?: {
         list: T[];
@@ -81,5 +84,5 @@ export interface AtomState<T> {
 }
 export declare function makeAtom<T>(): AtomState<T | undefined>;
 export declare function makeAtom<T>(initial: T): AtomState<T>;
-export declare const useAtom: <T>(atom: AtomState<T>) => T;
+export declare function useAtom<T>(atom: AtomState<T>): T;
 export {};
